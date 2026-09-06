a9bd4f43bf99d6a4b99de8b57913825ddb603583
depadangkall <devadattadarmaputra@gmail.com>
2026-09-07 03:07:29 +0700

    feat(tugas1): implement experience, education, moments

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -145,7 +145,7 @@ WHITENOISE_USE_FINDERS = True
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
-#121212121212122
+# For Mail
 MAILERS = {
     'default': {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
